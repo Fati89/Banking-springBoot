1dc1a74c09c855134a0c793a8592a6d0217a3db4
Fati89 <fatimazahraee989@gmail.com>
2026-05-01 08:10:29 +0100

    init

---
+++ src/main/java/org/example/ebankingbe/EbankingBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ebankingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ebankingbe/EbankingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ebankingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
